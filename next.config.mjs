b0ed17c4fae6a32836236314fb249ae3b51979b8
Milton Bolonha <miltonbolonha@gmail.com>
2024-10-02 18:54:19 -0300

    feature/git init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  reactStrictMode: false,
+  output: "export",
+  trailingSlash: true,
+  swcMinify: true,
+  assetPrefix: isProd ? process.env.WEBSITE_URL : "",
+  images: {
+    deviceSizes: [350, 480, 640, 768, 960, 1250, 2048],
+    loader: "custom",
+    loaderFile: "./lib/image-loader.js",
+    formats: ["image/webp"],
+  },
+};
 
 export default nextConfig;
